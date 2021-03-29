2c839b77432dbce07b2304cc054e7a099ffb5c65
Lowell13 <romain.matteo@campus.academy>
2021-03-29 16:09:07 +0200

    Fix for Mathis

--- lib/qsn.dart
+++ lib/qsn.dart
@@ -8,75 +8,30 @@ class Qsn extends StatelessWidget {
             child: Scaffold(
                 body: ListView(
                     children: [
-                      Container(padding: EdgeInsets.all(10.0),
-                          height: MediaQuery.of(context).size.height/3,
-                          width: MediaQuery.of(context).size.width,
-                          child: Column(mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Text('Qui sommes nous?', style: Theme.of(context).textTheme.headline1),
-                              Text('The N.E.C est un challenge de réflexion pour la nouvelle génération suivi d’un défi d’action et de dépassement de soi ouvert à tous.', style: Theme.of(context).textTheme.bodyText1)
-                            ]
-                          )),
-                      Container(padding: EdgeInsets.all(10.0),
-                          height: MediaQuery.of(context).size.height/3,
-                          width: MediaQuery.of(context).size.width,
-                          child: Column(mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Text('Nos Valeurs', style: Theme.of(context).textTheme.headline1),
-                              Text('bla bla bla', style: Theme.of(context).textTheme.bodyText1)])),
-                      Container(padding: EdgeInsets.all(10.0),
-                        height: MediaQuery.of(context).size.height/3,
-                        width: MediaQuery.of(context).size.width,
-                          child: Column(mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Text('Nos Partenaires', style: Theme.of(context).textTheme.headline1),
-                              GridView(
-                                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                                    crossAxisCount: 2,
-                                    childAspectRatio: 0.9,
-                                  ),
-                              children: [
-                                Image.asset('assets/Logos/awft_logo.png', width: 100, height: 100,),
-                                Image.asset('assets/Logos/cropped-LogoRESPIRE.png', width: 100, height: 100,),
-                                Image.asset('assets/Logos/G4.jpg', width: 100, height: 100,),
-                                Image.asset('assets/Logos/logo-amos-noir.png', width: 100, height: 100,),
-                                Image.asset('assets/Logos/logo-Atlas.png', width: 100, height: 100,),
-                                Image.asset('assets/Logos/LOGO_CAMPUS_ACADEMY.png', width: 100, height: 100,),
-                                Image.asset("assets/Logos/AT.png", width: 100, height: 100,),
-                                Image.asset("assets/Logos/S-Camp-Logo.png", width: 100, height: 100,),
-                                Image.asset("assets/Logos/Tripwiz.png", width: 100, height: 100,),
-                              ],
-                              ),
-                         /*    Column(
-                                    children: [
-                                      Container(
-                                      width: 50,
-                                      height: 50,
-                                      decoration: BoxDecoration(
-                                          image: DecorationImage(
-                                              image: AssetImage("assets/Logos/AT.png")
-                                          )
-                                      ),
-                                    ),
-                                      Image.asset('assets/Logos/awft_logo.png', width: 100, height: 100,),
-                                      Image.asset('assets/Logos/cropped-LogoRESPIRE.png'),
-                                      Image.asset('assets/Logos/G4.jpg'),
-                                      Image.asset('assets/Logos/logo-amos-noir.png'),
-                                      Image.asset('assets/Logos/logo-Atlas.png'),
-                                      Image.asset('assets/Logos/LOGO_CAMPUS_ACADEMY.png'),
-                                    ],
-                              ),*/
-                              ],
-                              ),
-                            ),
+                      Column(mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text('Qui sommes nous?', style: Theme.of(context).textTheme.headline1),
+                          Padding(
+                            //padding: const EdgeInsets.all(8.0),
+                            padding: const EdgeInsets.fromLTRB(8.0, 20.0, 8.0, 20.0),
+                            child: Text('The N.E.C est un challenge de réflexion pour la nouvelle génération suivi d’un défi d’action et de dépassement de soi ouvert à tous.', style: Theme.of(context).textTheme.bodyText1),
+                          ),
+                          Text('Nos Valeurs', style: Theme.of(context).textTheme.headline1),
+                          Text('bla bla bla', style: Theme.of(context).textTheme.bodyText1),
+                          Text('Nos Partenaires', style: Theme.of(context).textTheme.headline1),
+                          Image.asset("assets/Logos/AT.png", width: 100, height: 100),
+                          Image.asset('assets/Logos/awft_logo.png', width: 100, height: 100,),
+                          Image.asset('assets/Logos/cropped-LogoRESPIRE.png'),
+                          Image.asset('assets/Logos/G4.jpg'),
+                          Image.asset('assets/Logos/logo-amos-noir.png'),
+                          Image.asset('assets/Logos/logo-Atlas.png'),
+                          Image.asset('assets/Logos/LOGO_CAMPUS_ACADEMY.png'),
+                        ],
+                      ),
                     ]
                 )
-
-
             )
-
         )
-
     );
   }
 }
