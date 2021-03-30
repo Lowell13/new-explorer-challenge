cc91a8dd3f5e2470a69eed19c2993cc1abcc9ad6
mromain <romain.matteo13@gmail.com>
2021-03-30 18:04:43 +0200

    Redirection to listPage

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:nec/connection/connection.dart';
 import 'package:nec/pages/DetailPage.dart';
+import 'package:nec/pages/ListPage.dart';
 import 'package:nec/proximity/Proximity.dart';
 import 'package:nec/score/score.dart';
 import '../qsn/qsn.dart';
@@ -22,6 +23,9 @@ Map<String, WidgetBuilder> routes = {
   },
   '/proximity': (context) {
     return Proximity();
+  },
+  '/listPage': (context) {
+    return ListPage();
   },
   '/detailPage': (context) {
     return DetailPage();
