0a43327a948cc4393cd09b33f202292108bb5245
Mathis Doré <mathis.dore4@gmail.com>
2021-03-30 13:23:24 +0200

    Logos partenaires en carousel

--- lib/qsn/qsn.dart
+++ lib/qsn/qsn.dart
@@ -1,6 +1,9 @@
+import 'package:carousel_slider/carousel_options.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:nec/widget/drawer.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+
 
 
 class Qsn extends StatelessWidget {
@@ -43,20 +46,86 @@ class Qsn extends StatelessWidget {
                           Padding(
                             padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
                             child: Text('Nos Partenaires', style: Theme.of(context).textTheme.headline1),),
-                          GridView.count(
-                              shrinkWrap: true,
-                              crossAxisCount: 2,
-                              physics: ScrollPhysics(),
-                              children: [
-                                Image.asset("assets/Logos/AT.png", width: 100, height: 100),
-                                Image.asset('assets/Logos/awft_logo.png', width: 100, height: 100,),
-                                Image.asset('assets/Logos/cropped-LogoRESPIRE.png', width: 100, height: 100,),
-                                Image.asset('assets/Logos/G4.jpg', width: 100, height: 100,),
-                                Image.asset('assets/Logos/logo-amos-noir.png', width: 100, height: 100,),
-                                Image.asset('assets/Logos/logo-Atlas.png', width: 100, height: 100,),
-                                Image.asset('assets/Logos/LOGO_CAMPUS_ACADEMY.png', width: 100, height: 100,),
-                              ]
+                          CarouselSlider(items: [ Container(
+                            margin: EdgeInsets.fromLTRB(10.0,55.0,10.0,55.0),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8.0),
+                              image: DecorationImage(
+                                image : AssetImage("assets/Logos/AT.png"),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
                           ),
+                            Container(
+                              margin: EdgeInsets.all(0.0),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8.0),
+                                image: DecorationImage(
+                                  image : AssetImage("assets/Logos/awft_logo.png"),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.fromLTRB(20.0,0.0,20.0,0.0),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8.0),
+                                image: DecorationImage(
+                                  image : AssetImage("assets/Logos/cropped-LogoRESPIRE.png"),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.all(6.0),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8.0),
+                                image: DecorationImage(
+                                  image : AssetImage("assets/Logos/G4.jpg"),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.fromLTRB(0.0,26.0,0.0,27.0),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(0.0),
+                                image: DecorationImage(
+                                  image: AssetImage("assets/Logos/logo-amos-noir.png"),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.fromLTRB(0.0,40.0,0.0,40.0),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8.0),
+                                image: DecorationImage(
+                                  image : AssetImage("assets/Logos/logo-Atlas.png"),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.fromLTRB(75.0,0.5,75.0,0.5),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8.0),
+                                image: DecorationImage(
+                                  image : AssetImage("assets/Logos/LOGO_CAMPUS_ACADEMY.png"),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),],
+                            options: CarouselOptions(
+                            height: 180.0,
+                            enlargeCenterPage: true,
+                            autoPlay: true,
+                            aspectRatio: 16 / 9,
+                            autoPlayCurve: Curves.fastOutSlowIn,
+                            enableInfiniteScroll: true,
+                            autoPlayAnimationDuration: Duration(milliseconds: 800),
+                            viewportFraction: 0.8,
+                          ),),
                         ],
                       ),
                     ]
